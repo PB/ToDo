84d15eb9d2f1d54d502ee94448134bcc79c7bb90
Piotr B <b.piotr@gmail.com>
2015-11-21 23:14:36 +0100

    Added procfile v4

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec thin start -p $PORT -e $RACK_ENV
-web: bundle exec thin -R socky/config.ru -p $PORT start
+ws: bundle exec thin -R socky/config.ru -p $PORT start
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   end
 
   def set_ws
-    Socky::Client.new('http://0.0.0.0:3001/http/to_do', 'to_do_secret_123')
+    Socky::Client.new('https://murmuring-sands-8669.herokuapp.com/http/to_do', 'to_do_secret_123')
   end
 
   def create_task(task)
